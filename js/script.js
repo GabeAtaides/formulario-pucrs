077336ba6ea4688d0f6c6b1d6956a647a2b3429b
GabeAtaides <gabriel.ataides2@gmail.com>
2024-07-04 15:20:54 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Adicione aqui a lógica de login
+    alert('Formulário enviado!');
+});
